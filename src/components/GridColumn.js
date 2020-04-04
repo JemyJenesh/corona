d5d90e66b8d77ca06b07e181110850cbfac02b7a
JemyJenesh <xemejenesh@gmail.com>
2020-04-04 13:03:37 +0545

    grid-col component added

---
+++ src/components/GridColumn.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Statistic, Header, Grid } from "semantic-ui-react";
+import StatItem from "./StatItem";
+
+const GridColumn = ({
+  title,
+  data: { totalDeaths, activeCases, totalRecovered, totalCases },
+}) => {
+  return (
+    <Grid.Column>
+      <Header as="h1" textAlign="center">
+        Cases in the {title}:
+      </Header>
+      <Statistic.Group horizontal>
+        <StatItem color="red" label="Deaths" data={totalDeaths} />
+        <StatItem color="yellow" label="Active Cases" data={activeCases} />
+        <StatItem color="green" label="Recovered" data={totalRecovered} />
+        <StatItem color="grey" label="Total Cases" data={totalCases} />
+      </Statistic.Group>
+    </Grid.Column>
+  );
+};
+
+export default GridColumn;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
-import {
-  Statistic,
-  Input,
-  Header,
-  Grid,
-  Loader,
-  Segment,
-} from "semantic-ui-react";
+import { Input, Grid, Loader, Segment } from "semantic-ui-react";
 
-import StatItem from "../components/StatItem";
 import { store } from "../store/contexts/store";
 import DataTable from "../components/DataTable";
+import GridColumn from "../components/GridColumn";
 
 const Home = () => {
   const {
@@ -30,62 +23,8 @@ const Home = () => {
     <>
       <Grid columns="equal" divided stackable>
         <Grid.Row>
-          <Grid.Column>
-            <Header as="h1" textAlign="center">
-              Cases in Nepal
-            </Header>
-            <Statistic.Group horizontal>
-              <StatItem
-                color="red"
-                label="Deaths"
-                data={nepalData.totalDeaths}
-              />
-              <StatItem
-                color="yellow"
-                label="Active Cases"
-                data={nepalData.activeCases}
-              />
-              <StatItem
-                color="green"
-                label="Recovered"
-                data={nepalData.totalRecovered}
-              />
-              <StatItem
-                color="grey"
-                label="Total Cases"
-                data={nepalData.totalCases}
-              />
-            </Statistic.Group>
-          </Grid.Column>
-          <Grid.Column>
-            <Header as="h1" textAlign="center">
-              Cases in the World:
-            </Header>
-            <Statistic.Group horizontal>
-              <StatItem
-                color="red"
-                label="Deaths"
-                data={coronaDataLoaded ? coronaData[0].totalDeaths : undefined}
-              />
-              <StatItem
-                color="yellow"
-                label="Active Cases"
-                data={coronaDataLoaded ? coronaData[0].activeCases : undefined}
-              />
-              <StatItem
-                color="green"
-                label="Recovered"
-                data={
-                  coronaDataLoaded ? coronaData[0].totalRecovered : undefined
-                }
-              />
-              <StatItem
-                color="grey"
-                label="Total Cases"
-                data={coronaDataLoaded ? coronaData[0].totalCases : undefined}
-              />
-            </Statistic.Group>
-          </Grid.Column>
+          <GridColumn title="Nepal" data={nepalData} />
+          <GridColumn title="World" data={coronaDataLoaded && coronaData[0]} />
         </Grid.Row>
       </Grid>
 
